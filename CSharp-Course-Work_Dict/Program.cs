2e8405103a83e4e2e89c63c247cace888b78a496
dev <dev@A232PC13>
2023-04-30 16:16:50 +0300

    Add project files.

---
+++ CSharp-Course-Work_Dict/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Course_Work_Dict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
