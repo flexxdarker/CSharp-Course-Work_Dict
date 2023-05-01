ffa638603a8159aedafe4cb96ce64bf2d1664916
flexxdarker <ksenzhakmax@gmail.com>
2023-05-01 22:13:04 +0300

    add search by translate

--- CSharp-Course-Work_Dict/Dictionaries.cs
+++ CSharp-Course-Work_Dict/Dictionaries.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
@@ -10,32 +11,56 @@ namespace CSharp_Course_Work_Dict
     internal class Dictionaries
     {
         private string name { get; set; }
-        Dictionary<string,List<string>> dictionaries { get; set;}
-        Dictionaries(string name, string word, List<string> translate) 
+        private Dictionary<string,List<string>> dictionaries { get; set;}
+        public Dictionaries(string name, string word, List<string> translate) 
         {
             this.name = name;
+            dictionaries = new Dictionary<string,List<string>>();
             dictionaries.Add(word, translate);
+        }
+        public Dictionaries(string name) 
+        {
+            this.name = name;
+            dictionaries = new Dictionary<string,List<string>>();
         }
         public void print()
         {
             Console.Clear();
             Console.WriteLine($"Name of dictionary: {name}");
-            foreach (var translate in dictionaries.Values)
+            foreach (KeyValuePair<string, List<string>> tmp in dictionaries)
             {
-                foreach(var word in dictionaries.Keys)
+                Console.Write($"Word {tmp.Key} -> ");
+                foreach (var translate in tmp.Value)
                 {
-                    Console.WriteLine($"Word {word} -> Translate {translate}");
+                    Console.WriteLine($"Translate {translate}");
                 }
             }
         }
-        public void addWordAndTranslate(string word, List<string> translate)
+        public void addWordAndTranslate()
         {
             Console.Clear();
-            dictionaries.Add(word, translate);
-            print();
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+            if (dictionaries.Keys.Contains(word))
+            {
+                Console.Write("Enter translate of word: ");
+                string value = Console.ReadLine();
+                dictionaries[word].Add(value);
+            }
+            else
+            {
+                Console.Write("Enter translate of word: ");
+                string value = Console.ReadLine();
+                List<string> translation = new List<string>();
+                translation.Add(value);
+                dictionaries.Add(word, translation);
+            }
         }
-        public void changeWord(string changeword)
+        public void changeWord()
         {
+            print();
+            Console.Write("Enter word to change");
+            string changeword = Console.ReadLine();
             List<string> tmptransalte = new List<string>();
             foreach(var trans in dictionaries.Values)
             {
@@ -49,9 +74,16 @@ namespace CSharp_Course_Work_Dict
                 dictionaries.Add(tmpWord, tmptransalte);
             }
         }
-        public void changeTranslate(List<string> translate)
+        public void SearchTranslate()
         {
-            
+            Console.WriteLine("Enter word to search: ");
+            string tmp = Console.ReadLine();
+            var result = dictionaries.Where(x => tmp.Contains(x.Key)).SelectMany(x => x.Value).ToList();
+            foreach(var res in result)
+            {
+                Console.WriteLine(res);
+            }
         }
+        
     }
 }
--- CSharp-Course-Work_Dict/Program.cs
+++ CSharp-Course-Work_Dict/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
+            string name = "Ua-En";
+            
+            Dictionaries dictionary = new Dictionaries(name);
+            int a = 0;
+            do
+            {
 
+                Console.WriteLine(@"1.Add word
+2.print
+3.search
+4.change
+0.exit");
+                a = Convert.ToInt32(Console.ReadLine());
+                switch(a)
+                {
+                    case 1:
+                        dictionary.addWordAndTranslate();
+                        break;
+                    case 2:
+                        dictionary.print();
+                        break;
+                    case 3:
+                        dictionary.SearchTranslate();
+                        break;
+                    case 4:
+                        dictionary.changeWord();
+                        break;
+                    case 0:
+                        a = -1;
+                        break;
+                    default:
+                        break;
+
+                }
+            }while(a != -1);
+            
         }
     }
 }
