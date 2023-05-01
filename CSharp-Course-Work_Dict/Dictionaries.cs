bfcd735e3662c75d340a9d18a828c2126ad31c62
flexxdarker <ksenzhakmax@gmail.com>
2023-05-01 19:30:03 +0300

    create ctor, print and adding words

--- CSharp-Course-Work_Dict/Dictionaries.cs
+++ CSharp-Course-Work_Dict/Dictionaries.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,49 @@ namespace CSharp_Course_Work_Dict
 {
     internal class Dictionaries
     {
+        private string name { get; set; }
+        Dictionary<string,List<string>> dictionaries { get; set;}
+        Dictionaries(string name, string word, List<string> translate) 
+        {
+            this.name = name;
+            dictionaries.Add(word, translate);
+        }
+        public void print()
+        {
+            Console.Clear();
+            Console.WriteLine($"Name of dictionary: {name}");
+            foreach (var translate in dictionaries.Values)
+            {
+                foreach(var word in dictionaries.Keys)
+                {
+                    Console.WriteLine($"Word {word} -> Translate {translate}");
+                }
+            }
+        }
+        public void addWordAndTranslate(string word, List<string> translate)
+        {
+            Console.Clear();
+            dictionaries.Add(word, translate);
+            print();
+        }
+        public void changeWord(string changeword)
+        {
+            List<string> tmptransalte = new List<string>();
+            foreach(var trans in dictionaries.Values)
+            {
+                tmptransalte.Add(trans.ToString());
+            }
+            if(dictionaries.Keys.Contains(changeword))
+            {
+                dictionaries.Remove(changeword);
+                Console.Write("Enter new Word: ");
+                string tmpWord = Console.ReadLine();
+                dictionaries.Add(tmpWord, tmptransalte);
+            }
+        }
+        public void changeTranslate(List<string> translate)
+        {
+            
+        }
     }
 }
