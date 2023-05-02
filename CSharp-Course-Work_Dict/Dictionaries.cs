d2d631635be89a4db311c3ef70700597325d4479
mka <mka@RIVNE.ITSTEP>
2023-05-02 19:05:17 +0300

    Menu

--- CSharp-Course-Work_Dict/Dictionaries.cs
+++ CSharp-Course-Work_Dict/Dictionaries.cs
@@ -10,31 +10,38 @@ namespace CSharp_Course_Work_Dict
 {
     internal class Dictionaries
     {
-        private string name { get; set; }
-        private Dictionary<string,List<string>> dictionaries { get; set;}
-        public Dictionaries(string name, string word, List<string> translate) 
+        public string name { get; set; }
+        private Dictionary<string, List<string>> dictionaries { get; set; }
+        public Dictionaries(string name, string word, List<string> translate)
         {
             this.name = name;
-            dictionaries = new Dictionary<string,List<string>>();
+            dictionaries = new Dictionary<string, List<string>>();
             dictionaries.Add(word, translate);
         }
-        public Dictionaries(string name) 
+        public Dictionaries(string name)
         {
             this.name = name;
-            dictionaries = new Dictionary<string,List<string>>();
+            dictionaries = new Dictionary<string, List<string>>();
         }
         public void print()
         {
             Console.Clear();
+            Console.WriteLine("=================");
             Console.WriteLine($"Name of dictionary: {name}");
             foreach (KeyValuePair<string, List<string>> tmp in dictionaries)
             {
-                Console.Write($"Word {tmp.Key} -> ");
+                Console.WriteLine();
+                Console.WriteLine($@"\tWord {tmp.Key}      
+   \t_||_
+   \t\  /
+   \t \/");
                 foreach (var translate in tmp.Value)
                 {
-                    Console.WriteLine($"Translate {translate}");
+                    Console.WriteLine($"\tTranslate {translate}");
                 }
             }
+            Console.WriteLine("================");
+            Console.Clear();
         }
         public void addWordAndTranslate()
         {
@@ -55,35 +62,90 @@ namespace CSharp_Course_Work_Dict
                 translation.Add(value);
                 dictionaries.Add(word, translation);
             }
+            Console.Clear();
         }
         public void changeWord()
         {
+            Console.Clear();
             print();
             Console.Write("Enter word to change");
             string changeword = Console.ReadLine();
             List<string> tmptransalte = new List<string>();
-            foreach(var trans in dictionaries.Values)
+            if (dictionaries.Keys.Contains(changeword))
             {
-                tmptransalte.Add(trans.ToString());
-            }
-            if(dictionaries.Keys.Contains(changeword))
-            {
+                foreach (var trans in dictionaries[changeword])
+                {
+                    tmptransalte.Add(trans.ToString());
+                }
+
                 dictionaries.Remove(changeword);
                 Console.Write("Enter new Word: ");
                 string tmpWord = Console.ReadLine();
                 dictionaries.Add(tmpWord, tmptransalte);
+                print();
             }
+            else
+            {
+                Console.WriteLine($"Don't contain: {changeword}");
+            }
+            Console.Clear();
+        }
+        public void changeTranslete()
+        {
+            Console.Clear();
+            print();
+            Console.Write("Enter word to change");
+            string changeword = Console.ReadLine();
+            List<string> tmptransalte = new List<string>();
+            if (dictionaries.Keys.Contains(changeword))
+            {
+                foreach (var trans in dictionaries[changeword])
+                {
+                    tmptransalte.Add(trans.ToString());
+                }
+
+                dictionaries.Remove(changeword);
+                Console.Write("Enter new Word: ");
+                string tmpWord = Console.ReadLine();
+                dictionaries.Add(tmpWord, tmptransalte);
+                print();
+            }
+            else
+            {
+                Console.WriteLine($"Don't contain: {changeword}");
+            }
+            Console.Clear();
+        }
+        public void DeleteWord()
+        {
+            Console.Clear();
+            print();
+            Console.Write("Enter word to delete");
+            string deleteword = Console.ReadLine();
+            if (dictionaries.Keys.Contains(deleteword))
+            {
+                foreach (var word in dictionaries[deleteword])
+                {
+                    dictionaries.Remove(deleteword);
+                }
+            }
+            else { Console.WriteLine($"Dictionary don't contains: {deleteword}"); }
+            Console.Clear();
         }
         public void SearchTranslate()
         {
+            Console.Clear();
+            print();
             Console.WriteLine("Enter word to search: ");
             string tmp = Console.ReadLine();
             var result = dictionaries.Where(x => tmp.Contains(x.Key)).SelectMany(x => x.Value).ToList();
-            foreach(var res in result)
+            foreach (var res in result)
             {
                 Console.WriteLine(res);
             }
+            Console.Clear();
         }
-        
+
+
     }
 }
